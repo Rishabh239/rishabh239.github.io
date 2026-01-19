c6908b57921460393d6fc2bd15d8712d1c2d0cc3
unknown <tripathirishabh91@gmail.com>
2026-01-18 22:34:47 -0500

    Simplify page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,20 +6,20 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Rishabh Tripathi | Bioinformatics & ML</title>
+        <title>Rishabh Tripathi | Portfolio</title>
         <meta name="description" content="M.S. Bioinformatics at Northeastern University. Building production NGS pipelines, ML models for drug response, and spatial transcriptomics tools." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#050505" />
         <link rel="icon" href="/favicon.ico" />
         
         {/* Open Graph */}
-        <meta property="og:title" content="Rishabh Tripathi | Bioinformatics & ML" />
+        <meta property="og:title" content="Rishabh Tripathi | Portfolio" />
         <meta property="og:description" content="Building production NGS pipelines and ML tools for translational research." />
         <meta property="og:type" content="website" />
         
         {/* Twitter */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Rishabh Tripathi | Bioinformatics & ML" />
+        <meta name="twitter:title" content="Rishabh Tripathi | Portfolio" />
       </Head>
       <Component {...pageProps} />
     </>
